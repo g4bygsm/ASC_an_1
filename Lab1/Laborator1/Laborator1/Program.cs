58dae8b583aece13711185bbae9c85211994d8f4
Gabriel Trip <tripgabriel30@gmail.com>
2025-10-27 17:05:50 +0200

    problema

--- Lab1/Laborator1/Laborator1/Program.cs
+++ Lab1/Laborator1/Laborator1/Program.cs
@@ -1,62 +1,25 @@
-﻿
-    static void Main()
+﻿    static void Main()
     {
-        Console.Write("Introduceți baza sursă (b1, între 2 și 16): ");
+        Console.Write("Introduceți baza initiala: ");
         int b1 = int.Parse(Console.ReadLine());
-        Console.Write("Introduceți baza destinație (b2, între 2 și 16): ");
+        Console.Write("Introduceți baza finala: ");
         int b2 = int.Parse(Console.ReadLine());
         Console.Write("Introduceți numărul în baza b1: ");
         string numar = Console.ReadLine().ToUpper();
-        string[] parti = numar.Split('.');
-        string parteIntreaga = parti[0];
-        string parteFractionara = (parti.Length > 1) ? parti[1] : "";
-        long valoareIntreaga = ConvertToDecimal(parteIntreaga, b1);
-        double valoareFractionara = ConvertFractionToDecimal(parteFractionara, b1);
-        double valoareTotala = valoareIntreaga + valoareFractionara;
-        string rezultat = ConvertFromDecimal(valoareIntreaga, b2);
-        string rezultatFrac = ConvertFractionFromDecimal(valoareFractionara, b2, 10);
-
-        if (rezultatFrac != "")
-            rezultat += "." + rezultatFrac;
-
-        Console.WriteLine($"\nRezultatul în baza {b2} este: {rezultat}");
+        long valoare10 = ConvertToDecimal(numar, b1);
+        string rezultat = ConvertFromDecimal(valoare10, b2);
+        Console.WriteLine($"\nNumărul {numar} din baza {b1} este {rezultat} în baza {b2}");
     }
+
     static long ConvertToDecimal(string numar, int baza)
     {
         const string cifre = "0123456789ABCDEF";
         long rezultat = 0;
-
         foreach (char c in numar)
         {
             int valoare = cifre.IndexOf(c);
-            if (valoare < 0 || valoare >= baza)
-            {
-                Console.WriteLine("Cifră invalidă pentru baza sursă!");
-                Environment.Exit(1);
-            }
             rezultat = rezultat * baza + valoare;
         }
-
-        return rezultat;
-    }
-    static double ConvertFractionToDecimal(string fractie, int baza)
-    {
-        const string cifre = "0123456789ABCDEF";
-        double rezultat = 0.0;
-        double putere = 1.0 / baza;
-
-        foreach (char c in fractie)
-        {
-            int valoare = cifre.IndexOf(c);
-            if (valoare < 0 || valoare >= baza)
-            {
-                Console.WriteLine("Cifră invalidă în partea fracționară!");
-                Environment.Exit(1);
-            }
-            rezultat += valoare * putere;
-            putere /= baza;
-        }
-
         return rezultat;
     }
 
@@ -74,19 +37,3 @@
         }
         return rezultat;
     }
-    static string ConvertFractionFromDecimal(double fractie, int baza, int precizie)
-    {
-        const string cifre = "0123456789ABCDEF";
-        string rezultat = "";
-
-        for (int i = 0; i < precizie && fractie > 0; i++)
-        {
-            fractie *= baza;
-            int cifra = (int)fractie;
-            rezultat += cifre[cifra];
-            fractie -= cifra;
-        }
-
-        return rezultat;
-    }
-
