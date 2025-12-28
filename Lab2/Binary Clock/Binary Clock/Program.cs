dd0b990f92d68a053ab071d79cf28a1c87dd4056
Gabriel Trip <tripgabriel30@gmail.com>
2025-12-28 16:06:26 +0200

    Binary

---
+++ Lab2/Binary Clock/Binary Clock/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+class BinaryClock 
+{ 
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            DateTime acum = DateTime.Now;
+            Console.WriteLine("======================================");
+            Console.WriteLine("            BINARY CLOCK              ");
+            Console.WriteLine("======================================");
+            Console.WriteLine();
+            string oreBinar = Convert.ToString(acum.Hour, 2).PadLeft(6, '0');
+            string minuteBinar = Convert.ToString(acum.Minute, 2).PadLeft(6, '0');
+            string secundeBinar = Convert.ToString(acum.Second, 2).PadLeft(6, '0');
+            Console.WriteLine($"  ORA:     {oreBinar}  ({acum.Hour:D2})");
+            Console.WriteLine($"  MINUTE:  {minuteBinar}  ({acum.Minute:D2})");
+            Console.WriteLine($"  SECUNDE: {secundeBinar}  ({acum.Second:D2})");
+            Console.WriteLine();
+            Console.WriteLine("======================================");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Lab2/Jocu' lu oros/Jocu' lu oros/Program.cs
@@ -0,0 +1,13 @@
+﻿void CountingSort(int[] arr5)
+{
+    for (int n = 0; n < arr5.Length; n++)
+        for (int i = 0; i < arr5[n]; i++)
+            Console.Write($"{n} ");
+    Console.WriteLine();
+}
+
+int[] arr4 = InitRandomArray(1000, 100);
+PrintArray(arr4);
+
+int[] arr5 = Frequency(arr4, 100);
+CountingSort(arr5);
